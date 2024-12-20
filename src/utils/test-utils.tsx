d2b2eceae1731f286678b3d76fd2f0829b92615d
Marek Trybuła <trybu001@gmail.com>
2024-12-20 18:27:40 +0100

    Add GlobalStyles into test-utils

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -2,8 +2,15 @@ import { render, RenderOptions } from '@testing-library/react'
 import { ReactNode } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '../styles/theme'
+import { GlobalStyles } from '../styles/GlobalStyles'
 
 const renderWithTheme = (ui: ReactNode, options?: RenderOptions) =>
-  render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>, options)
+  render(
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>{ui}</ThemeProvider>
+    </>,
+    options,
+  )
 
 export { renderWithTheme }
